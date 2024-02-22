360ab82318ed4f3a0e5f30e28f6364b3533fc9fd
Pavel Glavatskikh <Byba1100@mail.ru>
2024-02-22 22:31:06 +0400

    controller modification StudentController, getMapping(progress)

--- src/main/java/ru/glavatskikh/controller/StudentControllers.java
+++ src/main/java/ru/glavatskikh/controller/StudentControllers.java
@@ -6,11 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.glavatskikh.model.Grade;
 import ru.glavatskikh.model.Student;
+import ru.glavatskikh.model.Term;
 import ru.glavatskikh.services.DisciplineServices;
 import ru.glavatskikh.services.GradeServices;
 import ru.glavatskikh.services.StudentServices;
 import ru.glavatskikh.services.TermServices;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/students")
 @Controller
@@ -57,16 +61,29 @@ public class StudentControllers {
         return "redirect:/students";
     }
 
-    @PatchMapping("/progress")
-    public String progress(@RequestParam("idProgress") Long id, Model model,
-                           @ModelAttribute("student") Student student) {
-
-        Student studentDB = studentServices.findOne(id);
-        model.addAttribute("student", studentDB);
-        List<Grade> grades = studentDB.getGrades();
-        model.addAttribute("termList", termServices.getAll());
-        model.addAttribute("grades", grades);
-        System.out.println();
+    @GetMapping("/progress")
+    public String progress(@RequestParam(value = "id_progress", required = false) Long idStudent,
+                           @RequestParam(value = "id", required = false) Long idTerm,
+                           @ModelAttribute("student") Student student, Model model,
+                           @ModelAttribute("term") Term term) {
+        if (idTerm != null) {
+            Student studentDB = studentServices.findOne(idStudent);
+            model.addAttribute("student", studentDB);
+            List<Grade> grades = studentDB.getGrades();
+            System.out.println();
+            List<Grade> gradesTermId = grades
+                    .stream()
+                    .filter(n -> n.getTerm().getId().equals(idTerm))
+                    .collect(Collectors.toList());
+            model.addAttribute("grades", gradesTermId);
+            model.addAttribute("termList", termServices.getAll());
+        } else {
+            Student studentDB = studentServices.findOne(idStudent);
+            model.addAttribute("student", studentDB);
+            List<Grade> grades = studentDB.getGrades();
+            model.addAttribute("termList", termServices.getAll());
+            model.addAttribute("grades", grades);
+        }
         return "student/progress";
     }
 }
