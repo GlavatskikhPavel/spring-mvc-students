bd19524be346095730354a6e5bec75a2b57d544b
Pavel Glavatskikh <Byba1100@mail.ru>
2024-02-27 11:33:13 +0400

    added BCryptPasswordEncoder

--- src/main/java/ru/glavatskikh/controller/AuthController.java
+++ src/main/java/ru/glavatskikh/controller/AuthController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AuthController {
 
-    @GetMapping()
+    @GetMapping("/login")
     public String loginPage() {
-        return "login";
+        return "auth/login";
     }
 }
